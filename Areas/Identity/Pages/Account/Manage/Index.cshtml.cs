197ed9ade19f670e3f1d32b663e427b3198ec617
Japple97 <applejake9@gmail.com>
2023-02-24 16:39:10 -0500

    Add User Image (Avatar) to Profile Management

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -4,9 +4,11 @@
 
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using JABlog.Models;
+using JABlog.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,13 +19,16 @@ namespace JABlog.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<BlogUser> _userManager;
         private readonly SignInManager<BlogUser> _signInManager;
+        private readonly IImageService _imageService;
 
         public IndexModel(
             UserManager<BlogUser> userManager,
-            SignInManager<BlogUser> signInManager)
+            SignInManager<BlogUser> signInManager,
+            IImageService imageService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _imageService = imageService;
         }
 
         /// <summary>
@@ -52,6 +57,22 @@ namespace JABlog.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public class InputModel
         {
+            [Display(Name = "First Name")]
+            [StringLength(40, ErrorMessage = "The {0} must be at least {2} and at the most {1} characters", MinimumLength = 2)]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Last Name")]
+            [StringLength(40, ErrorMessage = "The {0} must be at least {2} and at the most {1} characters", MinimumLength = 2)]
+            public string LastName { get; set; }
+
+            //Properties for storing image
+            public byte[] ImageData { get; set; }
+            public string ImageType { get; set; }
+
+           
+            [NotMapped]
+            public virtual IFormFile ImageFile { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -70,7 +91,10 @@ namespace JABlog.Areas.Identity.Pages.Account.Manage
 
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                ImageData = user.ImageData
             };
         }
 
@@ -99,6 +123,21 @@ namespace JABlog.Areas.Identity.Pages.Account.Manage
                 await LoadAsync(user);
                 return Page();
             }
+
+            //custom code
+            user.FirstName = Input.FirstName;
+            user.LastName = Input.LastName;
+
+            if(Input.ImageFile != null)
+            {
+                user.ImageData = await _imageService.ConvertFileToByteArrayAsync(Input.ImageFile);
+                user.ImageType = Input.ImageFile.ContentType;
+            }
+
+            await _userManager.UpdateAsync(user);
+            //end custom code
+
+
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
             if (Input.PhoneNumber != phoneNumber)
